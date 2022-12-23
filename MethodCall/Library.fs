4649145136ca6c5fea194034d34251238536b2f1
Matthijs Prent <matthijs.prent@Applicos.com>
2022-12-23 08:28:05 +0100

    Add project files.

---
+++ MethodCall/Library.fs
@@ -0,0 +1,14 @@
+﻿namespace MethodCall
+
+type ThreeStrings  = {
+    first: string
+    second: string
+    third: string
+}
+
+module Methods =
+    let DirectCall (first: string) (second: string) =
+        first + second
+
+    let CallWithRecord (input: ThreeStrings) =
+        input.first + input.second + input.third
---
+++ TestMethodCall/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string firstResult = MethodCall.Methods.DirectCall("firstDirect", "secondDirect");
+            Console.WriteLine(firstResult);
+
+            string secondResult = MethodCall.Methods.CallWithRecord(new MethodCall.ThreeStrings("firstIndirect", "secondIndirect", "thirdIndirect"));
+            Console.WriteLine(secondResult);
+            
+        }
+    }
+}
